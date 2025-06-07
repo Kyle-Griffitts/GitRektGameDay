4e9983c489c45855de7c437b11e45c993a8d9bfb
Kyle Giffitts <204399760+Kyle-Griffitts@users.noreply.github.com>
2025-06-07 18:08:15 -0400

    updated to Tilemap

--- GitRekt Project 1/Assets/Scripts/Procedurally Generated levels/OutdoorMapGenerator.cs
+++ GitRekt Project 1/Assets/Scripts/Procedurally Generated levels/OutdoorMapGenerator.cs
@@ -1,20 +1,24 @@
 using System;
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 // In Unity. Edit -> Project Settings -> Graphics -> Camera -> Custom Axis -> x: 0; y: 1; z: -0.26 needs to be set to get the tiles to properly display
 
 public class OutdoorMapGenerator : MonoBehaviour
 {
+    public Tilemap tilemap;
+    public TileBase[] tiles;
 
-    public GameObject tilePrefab; // Assign your sprite tile prefab
-    [SerializeField] public int mapWidth = 10;
-    [SerializeField] public int mapHeight = 10;
-    [SerializeField] public float tileWidth = 1f;
-    [SerializeField] public float tileHeight = 0.5f;
+    [SerializeField] public int minMapWidth = 5;
+    [SerializeField] public int maxMapWidth = 20;
+    [SerializeField] public int minMapHeight = 5;
+    [SerializeField] public int maxMapHeight = 20;
+    
 
-
+    System.Random random;
 
     void Start()
     {
@@ -24,23 +28,23 @@ public class OutdoorMapGenerator : MonoBehaviour
     //Generate Map method
     void GenerateMap()
     {
+        tilemap.RefreshAllTiles();
+        tilemap.ClearAllTiles();
+        random = new System.Random();
+        
+        int mapWidth = random.Next(minMapWidth, maxMapWidth);
+        int mapHeight = random.Next(minMapHeight, maxMapHeight);
+
         // instantiate the Object or Sprite inside the method
         for (int x = 0; x < mapWidth ; x++)
         {
             for (int y = 0; y < mapHeight; y++)
             {
-                Vector2 isoPosition = ConvertToIsometric(x, y);
-                Instantiate(tilePrefab, new Vector3(isoPosition.x, isoPosition.y, 0), Quaternion.identity); // Quaternion is the rotation. Identity is default
+                int randomIndex = random.Next(tiles.Length);
+                TileBase randomTile = tiles[randomIndex];
+                tilemap.SetTile(new Vector3Int(x, y, 0), randomTile);
             }
         }
-    }
-
-    Vector2 ConvertToIsometric(int x, int y)
-    {
-        float isoX = (x - y) * tileWidth / 2;
-        float isoY = (x + y) * tileHeight / 2;
-        return new Vector2(isoX, isoY);
-   
     }
 
 }
